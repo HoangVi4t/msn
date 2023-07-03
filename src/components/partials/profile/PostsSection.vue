036de5e810309134a9ad09b8ea6c8fad66b9d6f8
unknown <hvj4444@gmail.com>
2023-07-03 22:50:33 +0700

    fix text outline in post

--- src/components/partials/profile/PostsSection.vue
+++ src/components/partials/profile/PostsSection.vue
@@ -2,32 +2,37 @@
   <div>
     <div class="mx-auto py-4">
       <div class="flex flex-wrap font-bold text-gray-100">
+
         <div class="text-gray-900 text-xl">Posts</div>
         <div class="bg-green-500 w-full h-1"></div>
 
         <div class="w-full mt-4" v-if="userStore.id == route.params.id">
-          <RouterLinkButton btnText="Create Post" color="green" url="/account/create-post"/>
+          <RouterLinkButton
+              btnText="Create Post"
+              color="green"
+              url="/account/create-post"
+          />
         </div>
       </div>
     </div>
 
-
     <div class="flex flex-wrap mb-4">
       <div v-for="post in postStore.posts" :key="post" class="my-1 px-1 w-full md:w-1/2 lg:w-1/2">
 
-        <div class=" border rounded-lg ">
+
+        <div class="rounded-lg border">
           <a href="#">
-            <img class="rounded-t-lg w-full" :src="postStore.postImage(post.image)" alt=""/>
+            <img class="rounded-t-lg w-full" :src="postStore.postImage(post.image)" alt="">
           </a>
           <div class="p-2 md:p-4">
-            <div class="text-ld">
+            <div class="text-lg">
               <router-link
                   :to="'/account/post-by-id/' + post.id"
                   class="
-                    underline
-                    text-blue-500
-                    hover:text-blue-600
-                  "
+                                    underline
+                                    text-blue-500
+                                    hover:text-blue-600
+                                "
               >
                 {{ post.title }}
               </router-link>
@@ -40,33 +45,33 @@
               <router-link
                   :to="'/account/edit-post/' + post.id"
                   class="
-                  mr-1
-                    bg-blue-500
-                    hover:bg-blue-700
-                    text-white
-                    text-sm
-                    font-bold
-                    py-1
-                    px-2
-                    rounded-full
-                  "
+                                    mr-1
+                                    bg-blue-500
+                                    hover:bg-blue-700
+                                    text-white
+                                    text-sm
+                                    font-bold
+                                    py-1
+                                    px-2
+                                    rounded-full
+                                "
               >
-                Edit Post
+                Edit post
               </router-link>
               <button
+                  @click="deletePost(post.title, post.id)"
                   class="
-                    bg-red-500
-                    hover:bg-red-700
-                    text-white
-                    text-sm
-                    font-bold
-                    py-1
-                    px-2
-                    rounded-full
-                  "
-                  @click="deletePost(post.title,post.id)"
+                                    bg-red-500
+                                    hover:bg-red-700
+                                    text-white
+                                    text-sm
+                                    font-bold
+                                    py-1
+                                    px-2
+                                    rounded-full
+                                "
               >
-                Delete Post
+                Delete
               </button>
             </div>
           </div>
@@ -79,23 +84,22 @@
 </template>
 
 <script setup>
-import RouterLinkButton from "../../global/RouterLinkButton.vue"
-import {usePostStore} from "../../../store/post-store";
-import {useUserStore} from '../../../store/user-store'
-import {onMounted} from "vue";
-
-import {useRoute} from 'vue-router'
+import RouterLinkButton from '../../global/RouterLinkButton.vue'
+import { usePostStore } from '@/store/post-store'
+import { useUserStore } from '@/store/user-store'
+import { useRoute } from 'vue-router'
 import Swal from 'sweetalert2'
 import axios from 'axios'
+import { onMounted } from 'vue'
 
 const route = useRoute()
 const postStore = usePostStore()
 const userStore = useUserStore()
 
-
 onMounted(async () => {
   await postStore.fetchPostsByUserId(route.params.id)
 })
+
 const deletePost = async (title, id) => {
 
   Swal.fire({
@@ -125,4 +129,3 @@ const deletePost = async (title, id) => {
   })
 }
 </script>
-
